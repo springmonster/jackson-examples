f043bbefbe03777372fc43e2806f9dd6791ab423
kuanghc <609779612@qq.com>
2023-09-08 16:29:46 +0800

    add README.adoc

--- src/main/java/com/jackson/json/Car.java
+++ src/main/java/com/jackson/json/Car.java
@@ -5,6 +5,8 @@ public class Car {
   private String brand = null;
   private int doors = 0;
 
+  private char[] chars;
+
   public String getBrand() {
     return this.brand;
   }
@@ -19,5 +21,13 @@ public class Car {
 
   public void setDoors(int doors) {
     this.doors = doors;
+  }
+
+  public char[] getChars() {
+    return this.chars;
+  }
+
+  public void setChars(char[] chars) {
+    this.chars = chars;
   }
 }
--- src/test/java/com/jackson/json/JsonParserTest.java
+++ src/test/java/com/jackson/json/JsonParserTest.java
@@ -3,7 +3,9 @@ package com.jackson.json;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class JsonParserTest {
@@ -19,12 +21,36 @@ class JsonParserTest {
   }
 
   @Test
-  void testFromStringTo() throws JsonProcessingException {
+  void given_string_when_convert_to_instances_then_success() throws JsonProcessingException {
     String jsonArray = "[{\"brand\":\"ford\"}, {\"brand\":\"Fiat\"}]";
     ObjectMapper objectMapper = new ObjectMapper();
     Car[] cars = objectMapper.readValue(jsonArray, Car[].class);
 
     assertEquals("ford", cars[0].getBrand());
     assertEquals("Fiat", cars[1].getBrand());
+  }
+
+  @Test
+  void given_string_when_convert_to_instance_by_java_type_then_success()
+      throws JsonProcessingException {
+    String json = "{\"brand\":\"ford\", \"doors\":5}";
+    ObjectMapper objectMapper = new ObjectMapper();
+    JavaType javaType = objectMapper.getTypeFactory().constructType(Car.class);
+    Car car = objectMapper.readValue(json, javaType);
+
+    assertEquals("ford", car.getBrand());
+    assertEquals(5, car.getDoors());
+  }
+
+  @Test
+  void given_string_when_convert_to_instances_by_java_type_then_success()
+      throws JsonProcessingException {
+    String jsonArray = "[{\"chars\":\"abcdef\"}, {\"chars\":\"Leet Code\"}]";
+    ObjectMapper objectMapper = new ObjectMapper();
+    Car[] cars = objectMapper.readValue(jsonArray, Car[].class);
+
+    assertEquals(Arrays.toString(new char[]{'a', 'b', 'c', 'd', 'e', 'f'}),
+        Arrays.toString(cars[0].getChars()));
+    assertEquals(Arrays.toString("Leet Code".toCharArray()), Arrays.toString(cars[1].getChars()));
   }
 }
